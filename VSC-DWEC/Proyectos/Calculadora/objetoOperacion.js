ebb86c82eec60613f08c6eac08d3b3891a03afcb
Ángel López Palacios <angellopezpalacios61@gmail.com>
2020-11-23 11:57:00 +0100

    calculadora terminada? 2

--- VSC-DWEC/Proyectos/Calculadora/objetoOperacion.js
+++ VSC-DWEC/Proyectos/Calculadora/objetoOperacion.js
@@ -18,10 +18,31 @@ export class Operacion {
                 case "/": this.resultado = this.numero1 / this.numero2; break;
                 case "*": this.resultado = this.numero1 * this.numero2; break;
                 case "%": this.resultado = this.numero1 % this.numero2; break;
+                case "EXP": this.resultado = this.numero1 * Math.pow(10, this.numero2); break;
+                case "^": this.resultado = Math.pow(this.numero1, this.numero2); break;
                 case "+/-": this.resultado = -1 * (this.numero1); break;
+                case "√": this.resultado = Math.sqrt(this.numero1); break;
+                case "x!": this.resultado = this.fncFactorial(this.numero1); break;
+                case "ln": this.resultado = Math.log(this.numero1); break;
+                case "log": this.resultado = Math.log10(this.numero1); break;
+                case "sin": this.resultado = Math.sin(this.numero1); break;
+                case "cos": this.resultado = Math.cos(this.numero1); break;
+                case "tan": this.resultado = Math.tan(this.numero1); break;
             }
             return this.resultado;
         }
+    }
+
+    //Funcion para realizar el factorial de un numero
+    fncFactorial = (num) => {
+        let simbolo=1;
+        let factorial = 1;
+        if(num<0){
+            num*=-1;
+            simbolo=-1;}
+        for (var i = 2; i <= num; i++)
+            factorial = factorial * i;
+        return simbolo*factorial;
     }
 
     //Funcion que reinicia los valores del objeto
