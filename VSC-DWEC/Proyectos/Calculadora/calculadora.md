98ca6e3c38b9614079fb5bf67443e1224db4de68
Ángel López Palacios <angellopezpalacios61@gmail.com>
2020-12-02 20:22:27 +0100

    calculadora y diw

--- VSC-DWEC/Proyectos/Calculadora/calculadora.md
+++ VSC-DWEC/Proyectos/Calculadora/calculadora.md
@@ -1,12 +1,17 @@
-import { Operacion } from './objetoOperacion.js';
-var operacion = new Operacion();
-var cajaTexto = document.getElementById("cajaTexto");
-cajaTexto.value = "0";
-var historial = document.getElementsByClassName("historial")[0];
-var teclasCent = document.getElementById("teclas-cientificas");
-var menosEsOperador = false;
+# Trabajo calculadora - Ángel López Palacios
 
-//Funcion que segun el valor que se reciba del evento 
+El proyecto se basa en realizar una calculadora funcional utilizando js con modulos y POO.
+
+***
+
+## Archivo calculadora.js
+
+En calculadora.js simplemente se inicializa el evento para cada tecla de la calculadora.
+Y el evento consistira en comprobar que tecla se pulso y segun la tarea que desempeñe en la calculadora realiza una funcion u otra.
+
+Funcion asociada al evento click que tienen todas la teclas:
+
+~~~JavaScript
 var escribir = (evento) => {
     let valor = evento.srcElement.value;
 
@@ -21,7 +26,7 @@ var escribir = (evento) => {
     //Variables booleanas que utilizan los arrays anteriores para determinar la funcion de la tecla en el switch.
     let escribirEnConsola = (valor >= 0 && valor <= 9) || ((valor === "-") && (!menosEsOperador)) || valoresEscribir.includes(valor);
     let operacionMonoNumero = valoresOperadorMono.includes(valor);
-    let operacionMultiNumero = ((valor == "-") && (menosEsOperador)) || valoresOperadorMulti.includes(valor);
+    let operacionMultiNumero = ((valor === "-") && (menosEsOperador)) || valoresOperadorMulti.includes(valor);
 
     switch (true) {
         case escribirEnConsola:
@@ -52,15 +57,28 @@ var escribir = (evento) => {
             break;
     }
 }
+~~~
 
-//Se añade la funcion escribir a cada boton. 
-var inicializarEventos = () => {
-    let botonAux;
-    let arrayBotones = document.getElementsByName("btn");
-    for (let i = 0; i < arrayBotones.length; i++) {
-        botonAux = arrayBotones[i];
-        botonAux.addEventListener("click", escribir);
-    }
-}
+***
 
-inicializarEventos();
+## Archivo objetoOperacion.js
+
+Este archivo contiene el objeto Operacion utilizado para realizar comprobaciones sobre los valores recibidos y tambien realizar operaciones matematicas.
+
+- Funciones para realizar operaciones
+    - fncOperacion
+    - fncFactorial
+    - fncReset
+    - fncResultado
+    - fncResultadoNoNumerico
+- Funciones de validacion de datos e interaccion con interfaz
+    - fncOperacionMonoNumero
+    - fncOperacionMultiNumero
+    - fncOperacionEncadenada
+    - fncIgual
+    - fncAC
+    - fncCent
+    - fncEscribirConsola
+    - fncReiniciarConsola
+    - fncHistorial
+    - getHistorial
--- VSC-DWEC/Proyectos/Calculadora/objetoOperacion.js
+++ VSC-DWEC/Proyectos/Calculadora/objetoOperacion.js
@@ -1,32 +1,20 @@
 export class Operacion {
-    //Variables de las operaciones de la calculadora.
-    numero1;
-    numero2;
-    operacion;
-    resultado;
-    tipoOperacion;
-
-    //Variables de la interaccion de la calculadora.
-    reset;
-    consola;
-    visibleCent;
-    historial;
 
     /*
-    Constructor de la clase que inicializa las variables del objeto.
+     *Constructor de la clase que inicializa las variables del objeto.
      */
     constructor() {
+        //Valores de la operacion.
         this.numero1 = undefined;
         this.numero2 = undefined;
         this.operacion = "";
         this.resultado = undefined;
-        this.tipoOperacion="";
+        this.tipoOperacion = "";
 
+        //Valores para la interaccion de la calculadora
         this.reset = false;
         this.consola = "0";
-
-        this.visibleCent = false;
-
+        this.visibleCent = "none";
         this.historial = "";
     }
 
@@ -69,12 +57,16 @@ export class Operacion {
             num *= -1;
             simbolo = -1;
         }
-        for (var i = 2; i <= num; i++)
-            factorial = factorial * i;
+        if (num < 171) {
+            for (var i = 2; i <= num; i++)
+                factorial = factorial * i;
+        } else {
+            factorial = Infinity;
+        }
         return simbolo * factorial;
     }
 
-    //Funcion que reinicia los valores del objeto
+    //Funcion que reinicia los valores de la operacion del objeto
     fncReset = () => {
         this.numero1 = undefined;
         this.numero2 = undefined;
@@ -85,52 +77,87 @@ export class Operacion {
     fncResultado = (numero) => {
         if (this.numero1 != undefined) {
             this.fncOperacion(numero);
-            this.fncHistorial(this.tipoOperacion);
+            this.resultado = this.fncResultadoNoEsNumerico(this.resultado);
+            if (!isNaN(this.resultado))
+                this.fncHistorial(this.tipoOperacion);
             this.fncReset();
             return this.resultado;
         }
     }
 
+    //Funcion que Comprueba si el resultado el numerico o infinito
+    fncResultadoNoEsNumerico = (valor) => {
+        let aux = "Resultado no numérico."
+        if (!isNaN(valor) && valor != "Infinity" && valor != "-Infinity") {
+            aux = valor;
+        }
+        if (valor === Infinity || valor === -Infinity) {
+            aux = "Resultado infinito.";
+        }
+        return aux;
+    }
 
 
-/* 
-Funciones que hacen de intermediarios entre los eventos de la interfaz y las funciones de calculos. 
-En estas funciones se realizan todas las comprobaciones para que las operaciones salgan correctamente 
-y se devuelven cadenas de texto para mostrar por la consola de la calculadora.
-*/
+
+
+    /* 
+    Funciones que hacen de intermediarios entre los eventos de la interfaz y las funciones de calculos. 
+    En estas funciones se realizan todas las comprobaciones para que las operaciones salgan correctamente 
+    y se devuelven cadenas de texto para mostrar por la consola de la calculadora.
+    */
+
+    //Funcion que prepara y realiza las operaciones que requieren de solo un numero para ser realizada
     fncOperacionMonoNumero = (valor) => {
-        if (!isNaN(this.consola) && this.consola != "Infinity"&& this.consola != "-Infinity") {
-            this.numero1 = parseFloat(this.consola);
+        let fltConsola = parseFloat(this.consola);
+        fltConsola = this.fncOperacionEncadenada(fltConsola);
+        if (!isNaN(this.consola) && this.consola != "Infinity" && this.consola != "-Infinity") {
+            this.numero1 = fltConsola;
             this.operacion = valor;
-            this.tipoOperacion="mono";
+            this.tipoOperacion = "mono";
             this.consola = this.fncResultado(0);
             this.reset = true;
         }
         return this.consola;
     }
 
-
-    //cuando pulse el boton de un operador se resetea el numero si o si
-    //si se dio al igual se resetear el numero o realizar una operacion con el
+   //Funcion que prepara y realiza las operaciones que requieren de de dos numero para ser realizada
     fncOperacionMultiNumero = (valor) => {
-        let consolaNumero = parseFloat(this.consola);
-
-        if (!isNaN(this.consola) && this.consola != "Infinity"&& this.consola != "-Infinity") {
-            if (this.numero1 != undefined && !this.reset) {
-                this.tipoOperacion="multi";
-                this.consola = this.fncResultado(consolaNumero);
-
-                consolaNumero = this.resultado;
-            }
+        let fltConsola = parseFloat(this.consola);
+        fltConsola = this.fncOperacionEncadenada(fltConsola);
+        if (!isNaN(this.consola) && this.consola != "Infinity" && this.consola != "-Infinity") {
             this.operacion = valor;
-            this.fncOperacion(consolaNumero);
+            this.fncOperacion(fltConsola);
             this.reset = true;
         }
 
         return this.consola;
     }
 
-    //Funcion que realiza la funciones de la calculadora
+    /*Funcion utilizada en las dos funciones anteriores 
+     *y es utilizada para realizar una operacion depues de otra sin necesidad de pulsar igual
+     */
+    fncOperacionEncadenada = (consolaNumero) => {
+        let aux = consolaNumero;
+        if (!isNaN(this.consola) && this.consola != "Infinity" && this.consola != "-Infinity" && (this.numero1 != undefined && !this.reset)) {
+            this.tipoOperacion = "multi";
+            this.consola = this.fncResultado(aux);
+
+            aux = this.resultado;
+        }
+        return aux;
+    }
+
+    //Funcion utilizada para terminar la operacion si es posible
+    fncIgual() {
+        if (!this.reset && this.operacion != "") {
+            this.tipoOperacion = "multi";
+            this.consola = this.fncResultado(parseFloat(this.consola));
+            this.reset = true;
+        }
+        return this.consola;
+    }
+
+    //Funcion que reinicia la calculadora
     fncAC = () => {
         this.fncReset();
         this.resultado = undefined;
@@ -138,26 +165,20 @@ y se devuelven cadenas de texto para mostrar por la consola de la calculadora.
         return this.consola;
     }
 
-    //Funcion que devuelve de la calculadora
+    //Funcion que devuelve si la interfaz de la calculadora es visible o no
     fncCent = () => {
-        if (this.visibleCent) {
-            this.visibleCent = false;
-        }
-        else {
-            this.visibleCent = true;
+        switch (this.visibleCent) {
+            case "flex":
+                this.visibleCent = "none";
+                break;
+            case "none":
+                this.visibleCent = "flex";
+                break;
         }
         return this.visibleCent;
     }
 
-    fncIgual() {
-        if (!this.reset && this.operacion != "") {
-            this.tipoOperacion="multi";
-            this.consola = this.fncResultado(parseFloat(this.consola));
-            this.reset = true;
-        }
-        return this.consola;
-    }
-
+    //Funcion con la cual se escriben en consola los numero o simbolos que se pueden mostrar por consola
     fncEscribirConsola = (valor) => {
         switch (true) {
             case (valor >= 0 && valor <= 9):
@@ -179,6 +200,7 @@ y se devuelven cadenas de texto para mostrar por la consola de la calculadora.
         return this.consola;
     }
 
+    //Funcion utilizada para sustituir el valor de la consola por el parametro recibido
     fncReiniciarConsola = (valor) => {
         if (cajaTexto.value == "0" || this.reset) {
             this.consola = valor;
@@ -186,17 +208,19 @@ y se devuelven cadenas de texto para mostrar por la consola de la calculadora.
         }
     }
 
+    //Funcion para almacenar la(s) ultima(s) operaciones realizadas
     fncHistorial = (tipo) => {
-        switch(tipo){
+        switch (tipo) {
             case "multi":
-        this.historial = `<h2>${this.numero1} ${this.operacion} ${this.consola} =</h2><h1>${this.resultado}</h1><br>`;
-            break;
+                this.historial = `<h2>${this.numero1} ${this.operacion} ${this.consola} =</h2><h1>${this.resultado}</h1><br>`;
+                break;
             case "mono":
-        this.historial = `<h2>${this.operacion} (${this.consola}) =</h2><h1>${this.resultado}</h1><br>`;
-            break;
-    }
+                this.historial += `<h2>${this.operacion} (${this.consola}) =</h2><h1>${this.resultado}</h1><br>`;
+                break;
+        }
     }
 
+    //Funcion que devuelve el atributo historial y se vacia el string
     getHistorial = () => {
         let retorno = this.historial;
         this.historial = "";
