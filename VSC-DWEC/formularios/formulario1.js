6ad91a58e75a6fb88f1ed2647c53e71d5e959d57
Angel <yuter61@gmail.com>
2021-01-29 09:06:49 +0100

    Formulario1 acabado

--- VSC-DWEC/formularios/formulario1.js
+++ VSC-DWEC/formularios/formulario1.js
@@ -10,22 +10,69 @@ let sexo2 = document.getElementsByName("sexo2");
 let fecha = document.getElementsByName("fecha")[0];
 
 let contenedor = document.getElementById("contenedor");
+let errores = document.getElementById("contenedorErrores");
+
 var clickFunc = () => {
-    contenedor.innerHTML="";
+    //inicio contenedores
+    contenedor.innerHTML = "Valores:<br>";
+    errores.innerHTML = "Errores:<br>";
+
+    //campos que se van a validar
+    txtNombre = nombre.value.match(/^[A-Za-z]+((\s[A-Za-z]+)+)?$/);
+    txtApellidos = apellidos.value.match(/^[A-Za-z]+((\s[A-Za-z]+)+)?$/);
+    txtSexo1 = sexo1.value;
+    txtFecha = fecha.value;
+    boolIdioma = false;
+    boolSexo2 = false;
+
+    //Validacion idioma
     for (let i = 0; i < idioma.length; i++)
-        if (idioma[i].checked)
-        contenedor.innerHTML += (`Valor del idioma: ${idioma[i].value}<br>`);
+        if (idioma[i].checked) {
+            contenedor.innerHTML += (`Valor del idioma: ${idioma[i].value}<br>`);
+            boolIdioma = true;
+        }
+    if (!boolIdioma)
+        errores.innerHTML += "El valor del campo idioma no es valido<br>"
 
-            contenedor.innerHTML += `Valor del conector: ${conector.value}<br>`;
-            contenedor.innerHTML += `Valor del nombre: ${nombre.value}<br>`;
-            contenedor.innerHTML += `Valor de los apellidos: ${apellidos.value}<br>`;
-            contenedor.innerHTML += `Valor del sexo1: ${sexo1.value}<br>`;
+    //Validacion conector
+    contenedor.innerHTML += `Valor del conector: ${conector.value}<br>`;
 
-    for (let i = 0; i < sexo2.length; i++)
+    //Validacion nombre
+    if (txtNombre != null)
+        contenedor.innerHTML += `Valor del nombre: ${txtNombre[0]}<br>`;
+    else
+        errores.innerHTML += "El valor del campo nombre no es valido<br>"
+
+    //Validacion apellidos
+    if (txtApellidos != null)
+        contenedor.innerHTML += `Valor de los apellidos: ${txtApellidos[0]}<br>`;
+    else
+        errores.innerHTML += "El valor del campo apellidos no es valido<br>"
+
+    //Validacion sexo1
+    if (txtSexo1 != "")
+        contenedor.innerHTML += `Valor del sexo1: ${sexo1.value}<br>`;
+    else
+        errores.innerHTML += "El valor del campo sexo1 no es valido<br>"
+
+    //Validacion sexo2
+    for (let i = 0; i < sexo2.length; i++) {
         if (sexo2[i].checked)
-        contenedor.innerHTML += (`Valor del sexo2 seleccionado: ${sexo2[i].value}<br>`);
+            contenedor.innerHTML += (`Valor del sexo2 seleccionado: ${sexo2[i].value}<br>`);
+        boolSexo2 = true;
+    }
+    if (!boolSexo2)
+        errores.innerHTML += "El valor del campo sexo2 no es valido<br>"
 
+    //Validacion fecha
+    if (txtFecha != "")
         contenedor.innerHTML += `Valor de la fecha: ${fecha.value}<br>`;
+    else
+        errores.innerHTML += "El valor del campo fecha no es valido<br>"
+
+    //fin contenedores
+    contenedor.innerHTML += "<br>";
+    errores.innerHTML += "<br>";
 }
 
 enviar.onclick = clickFunc;
